b279bd298adf2b9486773bf427d2db019536ece8
ckenergy 程楷 <ckenergy@hellotalk.com>
2022-06-14 18:33:15 +0800

    更新readme

--- app/src/main/java/com/ckenergy/trace/MainActivity.java
+++ app/src/main/java/com/ckenergy/trace/MainActivity.java
@@ -5,21 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import com.ckenergy.lib.NoTrace;
-import com.ckenergy.lib.TestIgnore;
-
-import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.checkerframework.checker.nullness.qual.EnsuresNonNull;
-
 import androidx.annotation.RequiresApi;
-import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 
-import io.leangen.geantyref.TypeFactory;
+import com.ckenergy.lib.TestIgnore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -39,7 +31,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                long start = System.currentTimeMillis();
                 doSome1();
+                Log.e("main", "doSome time:"+(System.currentTimeMillis() - start));
             }
         });
     }
@@ -60,51 +54,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TestIgnore.INSTANCE.doSomeInIgnore("info");
     }
 
-    @org.checkerframework.checker.nullness.qual.EnsuresNonNull.List({@EnsuresNonNull({"this.preferences"}), @EnsuresNonNull({"this.monitoringSample"})})
-    @WorkerThread
-    @TestList({@NoTrace(1), @NoTrace(2)})
+    @TestList({@TestTrace(value = {"2", "3"}, type = {TraceType.NORMAL},
+            intRes = {1,2},
+            boolRes = {false, true},
+            shortRes = {3,4},
+            longRes = {5,6},
+            doubleRes = {7.0,8.0},
+            floatRes = {9.0f,10.0f},
+            byteRes = {10,11},
+            chatRes = {'a','b'},
+            byteType = 1, shortType = 2, longType = 3, charType = 'c', booleanType = true, floatType = 5.5f, doubleType = 6.6),
+
+            @TestTrace(value = {"4", "5"}, type = {TraceType.NORMAL},
+            intRes = {1,2},
+            boolRes = {false, true},
+            shortRes = {3,4},
+            longRes = {5,6},
+            doubleRes = {7.0,8.0},
+            floatRes = {9.0f,10.0f},
+            byteRes = {10,11},
+            chatRes = {'a','b'},
+            byteType = 1, shortType = 2, longType = 3, charType = 'c', booleanType = true, floatType = 5.5f, doubleType = 6.6)})
     private void doSome1() {
         Log.e("main", "doSome1");
-
-//        NoTrace noTrace1 = new NoTrace(){
-//
-//            @Override
-//            public Class<? extends Annotation> annotationType() {
-//                return NoTrace.class;
-//            }
-//
-//            @Override
-//            public int value() {
-//                return 1;
-//            }
-//        };
-//
-//        NoTrace noTrace2 = new NoTrace(){
-//
-//            @Override
-//            public Class<? extends Annotation> annotationType() {
-//                return NoTrace.class;
-//            }
-//
-//            @Override
-//            public int value() {
-//                return 3;
-//            }
-//        };
-//
-//        NoTrace[] list = new NoTrace[2];
-//        list[0] = noTrace1;
-//        list[1] = noTrace2;
-//
-//        HashMap<String, Object> map = new HashMap<>();
-//        map.put("value", list);
-//
-//        try {
-//            Annotation annotation = TypeFactory.annotation(TestList.class, map);
-//            Log.e("main", "doSome1 annotation:"+annotation);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
 
     }
 
--- app/src/main/java/com/ckenergy/trace/TestList.java
+++ app/src/main/java/com/ckenergy/trace/TestList.java
@@ -1,13 +1,11 @@
 package com.ckenergy.trace;
 
-import com.ckenergy.lib.NoTrace;
-
 /**
  * Created by chengkai on 2022/6/14.
  */
 
 public @interface TestList {
 
-    NoTrace[] value();
+    TestTrace[] value();
 
 }
